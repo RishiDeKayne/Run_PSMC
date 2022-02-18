1de6c7d4089e5f2dfc23f4127a5ea614cc453be6
Rishi De-Kayne <rishidek@hotmail.com>
2022-02-18 09:55:48 +0000

    Update psmc_fq_prepare.sh

--- psmc_fq_prepare.sh
+++ psmc_fq_prepare.sh
@@ -6,13 +6,17 @@
 # e.g. psmc_fq_prepare.sh Dchry2.2.fa bam.list /data/martin/genomics/analyses/Danaus_popgen/StHelena_project/psmc /scratch/rdekayne/psmc_full
 
 #run after loading genomics_general conda env with 'sconda genomics_general'
+#also run chmod+x and the script name to make executible
 
 #--------------------------------
+#this line defines a number of variables based on the 'values' provided when you execute the command
 GENOME=$1
 BAM_LIST=$2
 OUT_DIR=$3
 WORK_DIR=$4
 
+#this command will run a loop which goes through the list of bam files - extracts things like the bam name and location
+#and then 'echos' these and a number of commands into a text file which we can then run on the cluster in parallel - one job per bam file
 cat ${BAM_LIST} | while read line
 do
         echo 'processing' ${line}
